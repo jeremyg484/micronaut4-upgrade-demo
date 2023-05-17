8eac2ec090dc3793434cac97ad79f8bd0a713860
Jeremy Grelle <grellej@unityfoundation.io>
2023-05-17 18:26:35 -0400

    Use of PostConstruct annotation.

--- src/main/java/com/example/FooController.java
+++ src/main/java/com/example/FooController.java
@@ -3,10 +3,16 @@ package com.example;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 
+import javax.annotation.PostConstruct;
 import javax.validation.constraints.NotNull;
 
 @Controller
 public class FooController {
+
+	@PostConstruct
+	public void init() {
+
+	}
 
 	@Get("/foo/{id}")
 	public String getFoo(@NotNull String id) {
